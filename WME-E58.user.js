91c69bceeed07e7364d8d112f94dda1345d19b09
Anton Shevchuk <antonshevchuk@gmail.com>
2023-01-06 16:49:02 +0100

    Updated dependency

--- WME-E58.user.js
+++ WME-E58.user.js
@@ -14,7 +14,7 @@
 // @require      https://greasyfork.org/scripts/389765-common-utils/code/CommonUtils.js?version=1090053
 // @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1135567
 // @require      https://greasyfork.org/scripts/450221-wme-base/code/WME-Base.js?version=1129908
-// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1134661
+// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1135634
 // ==/UserScript==
 
 /* jshint esversion: 8 */
